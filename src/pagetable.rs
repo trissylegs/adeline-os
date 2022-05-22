aac7181d93b9072b541ed271492e90a7e1f4ac05
Triss Healy <th@trissyle.gs>
2022-05-22 23:38:08 +1000

    Fixed up printing page table

--- src/pagetable.rs
+++ src/pagetable.rs
@@ -3,10 +3,12 @@ use riscv::register::{self, satp::Mode};
 
 pub fn print_current_page_table() {
     let satp = register::satp::read();
-
-    println!("{:?}", satp.mode());
-    println!("{:?}", satp.asid());
-    println!("{:?}", satp.ppn());
+    
+    println!("PageTable: {{");
+    println!("  mode: {:?}", satp.mode());
+    println!("  asid: {:?}", satp.asid());
+    println!("  ppn:  {:?}", satp.ppn());
+    println!("}}");
     if satp.mode() == Mode::Bare {
         println!("Base mapping no more details.");
     }
