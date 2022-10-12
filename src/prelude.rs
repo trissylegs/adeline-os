cf403c3584471cd01f789d3c33814130d93884b9
Triss Healy <th@trissyle.gs>
2022-10-13 01:25:46 +1100

    Added more things to prelude.rs to be closer to std prelude.

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,3 +2,9 @@ pub use crate::print;
 pub use crate::println;
 pub use crate::time::rtc::TimeValue;
 pub use core::fmt::Write;
+
+pub use alloc::borrow::ToOwned;
+pub use alloc::boxed::Box;
+pub use alloc::string::String;
+pub use alloc::vec;
+pub use alloc::vec::Vec;
